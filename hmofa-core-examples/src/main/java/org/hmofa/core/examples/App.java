840ef4cab64c1002db134b9aabea5bb2f8cfc73a
hypoZhang <171852902@qq.com>
2018-11-13 14:17:52 +0800

    增加示例 模块，主要用于 存放一些 不确定，在研究过程中的内容

---
+++ hmofa-core-examples/src/main/java/org/hmofa/core/examples/App.java
@@ -0,0 +1,13 @@
+package org.hmofa.core.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,15 @@
+
+exception  -- 顶级，不依赖任何第三方库和模块
+
+lang	-- 依赖 exception ， 原则上不依赖第三方库
+
+
+resources   -- 依赖  exception、lang ， 可依赖第三方库
+
+
+
+
+
+
+
+
